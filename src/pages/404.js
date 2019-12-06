913a2fc9d6d5f143d7c6152f49fcaf115c1d2269
colpaerte <eli.colpaert@delaware.pro>
2019-12-06 13:36:16 +0100

    edit 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -17,15 +17,10 @@ class NotFoundPage extends React.Component {
 
         <div className="xl:w-10/12 mx-2 xl:mx-auto">
           <h1>Not Found</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-          <div className="flex flex-wrap">
-            <FullCoverCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-          </div>
+          <p className="text-4xl">
+            You just hit a route that doesn&#39;t exist... the sadness.
+          </p>
+          <div className="flex flex-wrap"></div>
         </div>
       </Layout>
     )
