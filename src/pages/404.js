e11842d2333eecc6cc72704e4bb3ee8ee37d20c3
Captain1Sock <kevin.vannimmen@gmail.com>
2020-01-16 13:46:31 +0100

    refactoring pages - vscode git test

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,25 +4,22 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = ({ props: data }) => {
+  const { title: siteTitle } = data.site.siteMetadata.title
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
+  return (
+    <Layout location={data.location} title={siteTitle}>
+      <SEO title="404: Not Found" />
 
-        <div className="xl:w-10/12 mx-2 xl:mx-auto">
-          <h1>Not Found</h1>
-          <p className="text-4xl">
-            You just hit a route that doesn&#39;t exist... the sadness.
-          </p>
-          <div className="flex flex-wrap"></div>
-        </div>
-      </Layout>
-    )
-  }
+      <div className="xl:w-10/12 mx-2 xl:mx-auto">
+        <h1>Not Found</h1>
+        <p className="text-4xl">
+          You just hit a route that doesn&#39;t exist... the sadness.
+        </p>
+        <div className="flex flex-wrap"></div>
+      </div>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
--- src/pages/product-list.js
+++ src/pages/product-list.js
@@ -4,24 +4,22 @@ import SEO from "../components/seo"
 import FacetContainer from "../components/FacetContainer"
 import ProductGrid from "../components/ProductGrid"
 
-class ProductListPage extends React.Component {
-  render() {
-    return (
-      <Layout location={this.props.location}>
-        <SEO title="Homepage" />
-        <main className="text-gray-900 antialiased bg-gray-300 w-full">
-          <div className="xl:w-10/12 mx-2 xl:mx-auto flex flex-col lg:flex-row">
-            <div className="lg:w-3/12 bg-white">
-              <FacetContainer />
-            </div>
-            <div className="lg:w-9/12">
-              <ProductGrid />
-            </div>
+const ProductListPage = location => {
+  return (
+    <Layout location={location}>
+      <SEO title="Homepage" />
+      <main className="text-gray-900 antialiased bg-gray-300 w-full">
+        <div className="xl:w-10/12 mx-2 xl:mx-auto flex flex-col lg:flex-row">
+          <div className="lg:w-3/12 bg-white">
+            <FacetContainer />
           </div>
-        </main>
-      </Layout>
-    )
-  }
+          <div className="lg:w-9/12">
+            <ProductGrid />
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default ProductListPage
