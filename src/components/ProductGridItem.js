6841c567222802e1cff0dfb27cbf39b226aacbc9
colpaerte <eli.colpaert@delaware.pro>
2020-01-22 11:23:31 +0100

    add powertools products in example hook

--- src/components/ProductGridItem.js
+++ src/components/ProductGridItem.js
@@ -1,17 +1,18 @@
 import React from "react"
 
-const ProductGridItem = () => {
+const ProductGridItem = ({ product }) => {
+  console.log("product", product)
   return (
     <div className="bg-white p-1 mr-8 rounded-sm">
       <img className="bg-cover" src="https://placehold.it/495x310" alt="" />
       <section className="px-4 py-4">
         <p className="text-peach-500 uppercase tracking-wide text-xs font-normal">
-          Leybold
+          {product.code}
         </p>
-        <h4 className="text-base font-normal tracking-wide">
-          Small Compact Vacuum Pump S 1.5
-        </h4>
-        <p className="text-gray-500 uppercase text-xs pt-4">SKU: 10101</p>
+        <h4 className="text-base font-normal tracking-wide">{product.name}</h4>
+        <p className="text-gray-500 uppercase text-xs pt-4">
+          €{product.price.value}
+        </p>
         <button className="flex items-center text-white mt-12 mb-2 group">
           <div className="bg-blue-400 group-hover:bg-peach-400 text-center font-medium py-4 px-2 rounded-l-sm">
             More information
