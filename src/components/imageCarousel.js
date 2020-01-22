67e144ce32b2edfdf94470eda77eb7119802c50b
vannimmenk <kevin.vannimmen@gmail.com>
2020-01-22 22:19:47 +0100

    fix http test data

--- src/components/imageCarousel.js
+++ src/components/imageCarousel.js
@@ -5,7 +5,7 @@ const ImageCarousel = () => {
     <div className="w-3/5 bg-gray-500">
       <img
         className="h-full w-full bg-cover"
-        src="http://placehold.it/1900x1080/FFF/8F8F8F"
+        src="https://placehold.it/1900x1080/FFF/8F8F8F"
       />
     </div>
   )
