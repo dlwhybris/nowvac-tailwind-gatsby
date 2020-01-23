4bc1c5790e77797923f512982939079db9132811
colpaerte <eli.colpaert@delaware.pro>
2020-01-23 16:00:00 +0100

    update left part

--- src/components/CheckoutStep.js
+++ src/components/CheckoutStep.js
@@ -1,12 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const CheckoutStep = () => {
+const CheckoutStep = ({ name, number, completed }) => {
   return (
-    <div>
-      <h3>Shipment / Pick Up Location</h3>
-      <Link>Edit</Link>
-      <button> OK</button>
+    <div className="flex justify-between bg-gray-300 py-4 my-4 w-5/6 px-6 rounded-lg content-center">
+      <h3 className="text-lg font-medium">{name}</h3>
+      {completed ? (
+        <div className="flex content-center">
+          <Link className="text-gray-600 font-light underline pr-4 text-xs">
+            Edit
+          </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            className="block w-6 h-6"
+          >
+            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" fill-rule="evenodd" />
+          </svg>
+        </div>
+      ) : (
+        <div>{number}</div>
+      )}
     </div>
   )
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -17,12 +17,38 @@ const CheckoutPage = props => {
       <div className="bg-gray-200">
         <div className="xl:w-10/12 mx-2 xl:mx-auto flex pb-32">
           <div className="checkoutOverview w-2/3">
-            <h1>CHECKOUT/ FINAL REVIEW</h1>
-            <h2>Secure Checkout</h2>
-            <CheckoutStep />
-            <CheckoutStep />
-            <CheckoutStep />
-            <CheckoutStep />
+            <div className="pt-12">
+              <h1 className="text-lg text-gray-800 font-thin">
+                CHECKOUT/ FINAL REVIEW
+              </h1>
+              <h2 className="text-3xl text-gray-900 tracking-wide font-medium">
+                Secure Checkout
+              </h2>
+            </div>
+            <div className="py-6">
+              <CheckoutStep
+                name="Shipment / Pick Up Location"
+                completed={true}
+                number="1"
+              />
+              <CheckoutStep
+                name="Shipping Method"
+                completed={true}
+                number="2"
+              />
+              <CheckoutStep
+                name="Payment & Billing Address"
+                completed={true}
+                number="3"
+              />
+              <CheckoutStep name="Final Review" completed={false} number="4" />
+              <div>
+                <div className="flex justify-between">
+                  <h4>Subtotal</h4>
+                  <span>$ 5.172,74</span>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="w-1/3  orderSummary bg-white shadow-lg p-8 pt-12">
             <div className="flex justify-between mb-4">
