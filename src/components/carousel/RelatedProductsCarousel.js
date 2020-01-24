8c9e268656adeeb75ae1f934fc36d0f50c984560
colpaerte <eli.colpaert@delaware.pro>
2020-01-24 15:35:06 +0100

    ProductGridItem

--- src/components/carousel/RelatedProductsCarousel.js
+++ src/components/carousel/RelatedProductsCarousel.js
@@ -1,26 +1,19 @@
 import React from "react"
 import ProductGridItem from "../ProductGridItem"
+import { usePowertoolsProducts } from "../../hooks/use-powertools-products"
 
 const RelatedProductsCarousel = () => {
+  const products = usePowertoolsProducts()
+
   return (
     <div className="w-10/12 py-16 m-auto">
       <h1 className="text-2xl">Simular Products</h1>
       <ul className="flex flex-wrap justify-between">
-        <div className="w-1/4 mb-12">
-          <ProductGridItem />
-        </div>
-
-        <div className="w-1/4 mb-12">
-          <ProductGridItem />
-        </div>
-
-        <div className="w-1/4 mb-12">
-          <ProductGridItem />
-        </div>
-
-        <div className="w-1/4 mb-12">
-          <ProductGridItem />
-        </div>
+        {products.map(entry => (
+          <div className="w-1/4 mb-12">
+            <ProductGridItem key={entry.node.code} product={entry.node} />
+          </div>
+        ))}
       </ul>
     </div>
   )
