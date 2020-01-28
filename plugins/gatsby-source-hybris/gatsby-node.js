be151e31178a26b54788161e68574720a7d251ef
colpaerte <eli.colpaert@delaware.pro>
2020-01-28 21:38:05 +0100

    review data included

--- plugins/gatsby-source-hybris/gatsby-node.js
+++ plugins/gatsby-source-hybris/gatsby-node.js
@@ -8,20 +8,28 @@ exports.sourceNodes = async (
   const apiUrl = `${options.url}?${queryString.stringify(
     options.searchParameters
   )}`
-  const response = await fetch(apiUrl)
-  const data = await response.json()
+  const productResponse = await fetch(apiUrl)
+  const productData = await productResponse.json()
 
-  data.products.forEach(product => {
-    createNode({
-      ...product,
-      id: createNodeId(`powertools-product-${product.code}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: "PowertoolsProduct",
-        content: JSON.stringify(product),
-        contentDigest: createContentDigest(product),
-      },
+  await Promise.all(
+    productData.products.map(async product => {
+      const reviewReponse = await fetch(
+        `https://api.c10zqj-delawarec1-d1-public.model-t.cc.commerce.ondemand.com/rest/v2/powertools/products/${product.code}/reviews?fields=DEFAULT`
+      )
+      const reviewData = await reviewReponse.json()
+      product.reviews = reviewData.reviews
+
+      createNode({
+        ...product,
+        id: createNodeId(`powertools-product-${product.code}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: "PowertoolsProduct",
+          content: JSON.stringify(product),
+          contentDigest: createContentDigest(product),
+        },
+      })
     })
-  })
+  )
 }
