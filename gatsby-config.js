0de771b2af80c632dba3a2370b35646bcb890e8d
Geert van Leemputten <geert.van.leemputten@gmail.com>
2020-02-12 14:42:03 +0100

    added algolia reset contentful setup to use dotenv added dotenv sample

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,75 @@
+const queries = require("./src/utils/algolia")
 // Load .env config
 const dotenv = require("dotenv")
 if (process.env.NODE_ENV !== "production") {
-  dotenv.config()
-}
-
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-      process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+    dotenv.config()
 }
 
 module.exports = {
-  siteMetadata: {
-    // edit below
-    title: `Manufacturing demo store`,
-    author: `Eli Colpaert`,
-    description: `Manufacturing demo store`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-  },
-  plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        tailwind: true,
-        purgeOnly: [`src/css/style.css`],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: contentfulConfig,
-    },
-    {
-      resolve: `gatsby-plugin-intl`,
-      options: {
-        path: `${__dirname}/plugins/gatsby-intl`,
-        languages: [`de`, `en`, `nl`],
-        defaultLanguage: `en`,
-        redirect: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Tailwind Starter`,
-        short_name: `FTS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#F7FAFC`,
-        display: `minimal-ui`,
+    siteMetadata: {
         // edit below
-        icon: `content/assets/logo.png`,
-      },
+        title: `Manufacturing demo store`,
+        author: `Eli Colpaert`,
+        description: `Manufacturing demo store`,
+        siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     },
-  ],
+    plugins: [
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-postcss`,
+        {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+                tailwind: true,
+                purgeOnly: [`src/css/style.css`],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
+            },
+        },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-algolia`,
+            options: {
+                appId: process.env.GATSBY_ALGOLIA_APP_ID,
+                apiKey: process.env.ALGOLIA_ADMIN_KEY,
+                queries,
+                chunkSize: 10000, // default: 1000
+            },
+        },
+        {
+            resolve: `gatsby-plugin-intl`,
+            options: {
+                path: `${__dirname}/plugins/gatsby-intl`,
+                languages: [`de`, `en`, `nl`],
+                defaultLanguage: `en`,
+                redirect: true,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Tailwind Starter`,
+                short_name: `FTS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#F7FAFC`,
+                display: `minimal-ui`,
+                // edit below
+                icon: `content/assets/logo.png`,
+            },
+        },
+    ],
 }
